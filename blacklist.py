3d962946239d8a0470684a47a3c8adb6097b2f33
maxnikulcha96 <85556872+maxnikulcha96@users.noreply.github.com>
2025-04-08 16:55:23 +0200

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -1,7 +1,37 @@
+import subprocess
 import sys
-import pydnsbl
+import concurrent.futures
+import json
 
-ip_checker = pydnsbl.DNSBLIpChecker()
+def install_pip(name):
+    subprocess.call([sys.executable, '-m', 'pip', 'install', name])
 
-result = ip_checker.check(sys.argv[1])
-print(result.blacklisted, end='')
+def check_ip(ip):
+    ip_checker = pydnsbl.DNSBLIpChecker()
+    try:
+        result = ip_checker.check(ip)
+        return ip, result.blacklisted
+    except:
+        return ip, None
+
+def main(ip_addresses):
+    results = {}
+    executor = concurrent.futures.ThreadPoolExecutor()
+    try:
+        future_to_ip = {executor.submit(check_ip, ip): ip for ip in ip_addresses}
+        for future in concurrent.futures.as_completed(future_to_ip):
+            ip, blacklisted = future.result()
+            results[ip] = blacklisted
+    finally:
+        executor.shutdown(wait=True)
+    print(json.dumps(results, indent=4, ensure_ascii=False))
+
+if __name__ == "__main__":
+    try:
+        import pydnsbl
+    except ImportError:
+        install_pip('pydnsbl')
+        import pydnsbl  # Re-import after install
+
+    ip_list = sys.argv[1:]
+    main(ip_list)
